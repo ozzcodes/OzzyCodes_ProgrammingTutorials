f3bb487d8c786b86ecd8d5ceb08ed7e1c9f27ad1
OzzyCodes <ajwaldro@gmail.com>
2019-01-20 16:19:18 -0500

    delete

--- CodingAcademy_Programs/MongoDB/MongoDB_2/dropIndex.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("127.0.0.1", 27017)
-db = client.LXF
-
-# Choose a collection
-someData = db.someData
-# Drop an index
-someData.drop_index("n_1")
