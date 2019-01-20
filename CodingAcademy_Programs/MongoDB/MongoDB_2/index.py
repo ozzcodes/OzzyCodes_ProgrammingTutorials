078d71065ae598cb4268179fed0d8bfe0cb8297c
OzzyCodes <ajwaldro@gmail.com>
2019-01-20 16:19:49 -0500

    delete

--- CodingAcademy_Programs/MongoDB/MongoDB_2/index.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient("127.0.0.1", 27017)
-db = client.LXF
-
-# Choose a collection
-someData = db.someData
-# Create the index
-someData.create_index("n")
