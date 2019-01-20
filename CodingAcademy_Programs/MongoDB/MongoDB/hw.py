17724b804d929605a0bbd382ed1114aa7c2bddef
OzzyCodes <ajwaldro@gmail.com>
2019-01-20 16:17:22 -0500

    delete hw.py

--- CodingAcademy_Programs/MongoDB/MongoDB/hw.py
+++
@@ -1,7 +0,0 @@
-from bottle import route, run, template
-
-@route('/hello/<user>')
-def index(user):
-    return template('<h2>Hello World from {{user}}!</h2>', user=user)
-    
-run(host='localhost', port=1234)
