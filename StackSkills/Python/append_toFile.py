6bfe56ceeba385c8f4b1422595f2fbf984744d31
OzzyCodes <ajwaldro@gmail.com>
2019-02-06 15:03:14 -0500

    del

--- StackSkills/Python/append_toFile.py
+++
@@ -1,9 +0,0 @@
-
-# append will add data to a file where write to file will overwrite any data to a file
-appendMe = 'Some more text here.'
-
-saveFile = open('exampleFile.txt', 'a')
-saveFile.write('\n')
-saveFile.write(appendMe)
-saveFile.close()
-
