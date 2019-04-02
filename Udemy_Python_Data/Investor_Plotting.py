a805211e4650c36e464d5207c4f15e0eb733ebb0
Austin Waldron <waldron.a@husky.neu.edu>
2019-04-02 11:32:03 -0400

    Add_Udemy Tutorials

---
+++ Udemy_Python_Data/Investor_Plotting.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  2 10:09:39 2019
+
+@author: ajwal
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+typeInvestor = {'Stocks', 'Bonds', 'Real Estate'}
+
