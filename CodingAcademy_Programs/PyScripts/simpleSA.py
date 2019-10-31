973d59c06dcc714c65e0ae81650f68d46bdfedbc
OzzyCodes <ajwaldro@gmail.com>
2019-10-31 12:14:26 -0400

    minor tweaks

--- CodingAcademy_Programs/PyScripts/simpleSA.py
+++ CodingAcademy_Programs/PyScripts/simpleSA.py
@@ -15,15 +15,14 @@ for line in sys.stdin:
     if host in counter:
         counter[host] += 1
     else:
-        counter[host] += 1
+        counter[host] = 1
 
-print("Processed ", total, "log records!")
+print("Processed ", total, " log records!")
 
 # the initial code doesn't sort the IP addresses in order
-'''
 for key in counter:
     print(key, ':', counter[key])
-'''
+
 # below is replacement to order the IP addresses
 for w in sorted(counter, key=counter.get, reverse=True):
     print(w, counter[w])
--- CodingAcademy_Programs/Times, Dates, Numbers/datetime_continuum.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from datetime import timedelta
-
-now = datetime.today()
-print(now)
-print(now + timedelta(days=1))
-
-then = now + timedelta(days=1)
-print(then - now)
